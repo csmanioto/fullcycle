549563cfd30c25dc26fdb6a9dc47877aa4278931
Carlos Smaniotto <csmanioto@gmail.com>
2023-04-14 12:48:57 -0300

    Add files via upload

---
+++ desafioNode/mysql/create_db.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE IF NOT EXISTS `nodedb`;
+
+use `nodedb`;
+
+CREATE TABLE IF NOT EXISTS `people` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
